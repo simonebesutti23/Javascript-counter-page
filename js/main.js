1120a1d242c6ba33bdc3286253f3d90f60358cd4
simonebesutti23 <besuttisimone23@gmail.com>
2024-06-10 00:30:53 +0200

    creazione titolo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 /*File js counter page with DOM manipulation*/
 /*Create the elements of the page only with dom manipulation*/
-cret
+/*structure of the page*/
+const container = document.createElement('div');
+/*Title creation*/
+const title = document.createElement('h1');
+const textNode = document.createTextNode("JAVASCRIPT COUNTER");
+title.appendChild(textNode);
+
+container.appendChild(title);
+
+document.body.appendChild(container);
